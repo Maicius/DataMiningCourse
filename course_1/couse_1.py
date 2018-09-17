83077fea20da3763ea5a26c9fd85f41cc85b0178
Maicius <maicius@outlook.com>
2018-09-17 22:25:40 +0800

    update

--- course_1/couse_1.py
+++ course_1/couse_1.py
@@ -34,21 +34,19 @@ diss_matrix = np.zeros((shape[0], shape[0]))
 # diss_data = diss_data.T + diss_data
 
 ### do it with broadcast, very quick
-# 转化为float
+# change str to float
 data = data.apply(lambda x: pd.to_numeric(x))
+# normalized data with max and min
+data = data.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
 for i in range(shape[0]):
     print(i)
     # compute dissimilarity matrix for sex
-    diss_matrix_sex[:, i] = (data[0] - data.iloc[i,0]) > 0
+    diss_matrix_sex[:, i] = np.abs(data[0] - data.iloc[i,0]) > 0
     # compute dissimilarity matrix for other attributes
     diss_matrix[:, i] = np.sum(np.abs(data.iloc[:,1:] - data.iloc[i,1:].T), axis=1)
 
 print(diss_matrix.shape, diss_matrix_sex.shape)
-diss_matrix_df = pd.DataFrame(diss_matrix)
-diss_matrix_sex_df = pd.DataFrame(diss_matrix_sex)
-# normalized data with max and min
-normalized_diss_matrix_df = diss_matrix_df.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
-dissimilarity_matrix = (diss_matrix_sex_df + normalized_diss_matrix_df) / 2
-
-dissimilarity_matrix.to_csv('dissimilarity_matrix.csv', header=None, index=False)
+dissimilarity_matrix = (diss_matrix_sex + diss_matrix) / 2
+dissimilarity_matrix_df = pd.DataFrame(dissimilarity_matrix)
+dissimilarity_matrix_df.to_csv('dissimilarity_matrix.csv', header=None, index=False)
 print(dissimilarity_matrix)
